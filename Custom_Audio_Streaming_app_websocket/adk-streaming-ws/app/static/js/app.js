8d469de250af3352bd33264c7cb988486a191c8a
enayetsyl <enayetflweb@gmail.com>
2025-12-04 12:32:52 +0600

    Fix WebSocket protocol mismatch for local development
    Change WebSocket URL from hardcoded wss:// to dynamically detect
    protocol based on window.location.protocol. This fixes the 'Invalid
    HTTP request received' warnings in uvicorn when running locally on
    http:// by using ws:// for HTTP connections and wss:// for HTTPS
    connections.

--- Custom_Audio_Streaming_app_websocket/adk-streaming-ws/app/static/js/app.js
+++ Custom_Audio_Streaming_app_websocket/adk-streaming-ws/app/static/js/app.js
@@ -1,18 +1,18 @@
 /**
-* Copyright 2025 Google LLC
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 /**
  * app.js: JS code for the adk-streaming sample app.
@@ -24,8 +24,9 @@
 
 // Connect the server with a WebSocket connection
 const sessionId = Math.random().toString().substring(10);
-const ws_url =
-  "wss://" + window.location.host + "/wss/" + sessionId;
+// Use ws:// for http:// and wss:// for https://
+const ws_protocol = window.location.protocol === "https:" ? "wss:" : "ws:";
+const ws_url = ws_protocol + "//" + window.location.host + "/ws/" + sessionId;
 let websocket = null;
 let is_audio = false;
 
